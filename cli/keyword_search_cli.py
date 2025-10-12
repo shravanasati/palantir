5c4572f1a203326dcb7f5a9efb7b5311ec56b4ec
Shravan Asati <dev.shravan@protonmail.com>
2025-10-13 00:05:30 +0530

    inverted index

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -3,12 +3,13 @@
 import argparse
 import sys
 from pathlib import Path
+
 # Add the project root to Python path
 project_root = Path(__file__).parent.parent
 sys.path.insert(0, str(project_root))
 
 # Import after path is set
-from src.keyword_search import search_keyword
+from src.keyword_search import InvertedIndex  # , search_keyword
 
 
 def main() -> None:
@@ -19,15 +20,31 @@ def main() -> None:
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
+    subparsers.add_parser("build", help="Build the inverted index")
+
     args = parser.parse_args()
 
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
-            for i, result in enumerate(search_keyword(args.query)):
-                print(f"{i+1}. {result}")
+            try:
+                ii = InvertedIndex.from_cache()
+            except FileNotFoundError:
+                print("index doesn't exist, run build command first")
+                exit(1)
+            # for i, result in enumerate(search_keyword(args.query)):
+            for i, result in enumerate(ii.get_documents(args.query)):
+                print(f"{i+1}. {result['title']}")
                 if i == 4:
                     break
+
+        case "build":
+            ii = InvertedIndex()
+            ii.build()
+            ii.save()
+            result = ii.get_documents("merida")
+            if len(result) > 0:
+                print("first document ID matching merida", result[0]["id"])
         case _:
             parser.print_help()
 
--- src/keyword_search.py
+++ src/keyword_search.py
@@ -1,4 +1,7 @@
-from src.load_dataset import get_stopwords, load_data
+from collections import defaultdict
+from pathlib import Path
+import pickle
+from src.load_dataset import get_stopwords, load_data, Movie
 
 from nltk import word_tokenize
 from nltk.stem import PorterStemmer
@@ -15,9 +18,68 @@ def preprocess_text(query: str) -> list[str]:
 
     stemmer = PorterStemmer()
     # tokenize, remove stopwords and stemming
-    query_tokens = [stemmer.stem(q) for q in word_tokenize(query) if q and q not in STOPWORDS]
+    query_tokens = [
+        stemmer.stem(q) for q in word_tokenize(query) if q and q not in STOPWORDS
+    ]
 
     return query_tokens
+
+
+class InvertedIndex:
+    def __init__(self) -> None:
+        self.documents = load_data()
+        self.index: dict[str, list[int]] = defaultdict(list)
+        self.docmap: dict[int, Movie] = {}
+
+    @staticmethod
+    def get_cache_paths():
+        """
+        Returns the filepaths to the cached index file and docmap files.
+        """
+        cache_dir = Path(__file__).parent.parent / "cache"
+        cache_dir.mkdir(parents=True, exist_ok=True)
+        index_file = cache_dir / "index.pkl"
+        docmap_file = cache_dir / "docmap.pkl"
+        return index_file, docmap_file
+
+    def _add_document(self, doc: Movie):
+        self.docmap[doc["id"]] = doc
+        tokens = preprocess_text(f"{doc['title']} {doc['description']}")
+        for tok in tokens:
+            self.index[tok].append(doc["id"])
+
+    def build(self):
+        for doc in self.documents:
+            self._add_document(doc)
+
+    def get_documents(self, query: str):
+        query_tokens = preprocess_text(query)
+        doc_ids = sorted([i for qtok in query_tokens for i in self.index.get(qtok, [])])
+        return [self.docmap[i] for i in doc_ids]
+
+    def save(self):
+        index_file, docmap_file = self.get_cache_paths()
+        with open(index_file, "wb") as f:
+            pickle.dump(self.index, f)
+        with open(docmap_file, "wb") as f:
+            pickle.dump(self.docmap, f)
+
+    @classmethod
+    def from_cache(cls):
+        ii = cls()
+        index_file, docmap_file = ii.get_cache_paths()
+        if index_file.exists() and docmap_file.exists():
+            with open(index_file, "rb") as iff:
+                ii.index = pickle.load(iff)
+            with open(docmap_file, "rb") as df:
+                ii.docmap = pickle.load(df)
+
+        else:
+            raise FileNotFoundError(
+                "cache/index.pkl or cache/docmap.pkl files don't exist"
+            )
+
+        return ii
 
 
 def search_keyword(query: str):
