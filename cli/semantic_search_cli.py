0eb02b4f1f2450aaaeae779108383159752f2462
Shravan Asati <dev.shravan@protonmail.com>
2025-11-03 18:32:42 +0000

    build embeddings and cache them

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -8,7 +8,7 @@ import sys
 project_root = Path(__file__).parent.parent
 sys.path.insert(0, str(project_root))
 
-from src.semantic_search import verify_model  # noqa: E402
+from src.semantic_search import embed_text, verify_embeddings, verify_model  # noqa: E402
 
 
 def main():
@@ -17,11 +17,23 @@ def main():
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     subparsers.add_parser("verify", help="Verify if the model is loaded.")
+    subparsers.add_parser("verify_embeddings", help="Verify if the embeddings are created.")
+
+    embed_parser = subparsers.add_parser(
+        "embed", help="Generate a text embedding"
+    )
+    embed_parser.add_argument("text", type=str, help="The text to embed.")
 
     args = parser.parse_args()
     match args.command:
         case "verify":
             verify_model()
+
+        case "embed":
+            embed_text(args.text)
+
+        case "verify_embeddings":
+            verify_embeddings()
 
         case _:
             parser.print_help()
--- src/semantic_search.py
+++ src/semantic_search.py
@@ -1,4 +1,8 @@
+from pathlib import Path
+import numpy as np
 from sentence_transformers import SentenceTransformer
+
+from src.load_dataset import Movie, load_data
 
 
 class SemanticSearch:
@@ -7,8 +11,80 @@ class SemanticSearch:
         self.model = SentenceTransformer("all-MiniLM-L6-v2")
         # model.encode(text)
 
+        self.embeddings = np.array([])
+        self.documents = []
+        self.document_map: dict[int, Movie] = {}
+
+    def generate_embedding(self, text: str):
+        text = text.strip()
+        if not text:
+            raise ValueError("empty text")
+
+        return self.model.encode([text])[0]
+
+    @staticmethod
+    def get_cache_paths():
+        cache_dir = Path(__file__).parent.parent / "cache"
+        cache_dir.mkdir(parents=True, exist_ok=True)
+
+        embeddings_file = cache_dir / "movie_embeddings.npy"
+        return embeddings_file
+
+    def build_embeddings(self, documents: list[Movie]):
+        self.documents = documents
+        self.document_map = {d["id"]: d for d in self.documents}
+        movies = [f"{d['title']}: {d['description']}" for d in documents]
+        self.embeddings = self.model.encode(movies, show_progress_bar=True)
+
+        embeddings_file_path = self.get_cache_paths()
+        np.save(embeddings_file_path, self.embeddings)
+
+        return self.embeddings
+
+    @classmethod
+    def from_cache(cls, documents: list):
+        ss = cls()
+        embeddings_file_path = ss.get_cache_paths()
+        if embeddings_file_path.exists():
+            ss.embeddings = np.load(embeddings_file_path)
+            if len(ss.embeddings) != len(documents):
+                raise ValueError("length mismatch")
+            ss.documents = documents
+            ss.document_map = {d["id"]: d for d in ss.documents}
+
+        else:
+            raise FileNotFoundError(
+                "cache/index.pkl or cache/docmap.pkl or cache/term_frequencies.pkl or cache/doc_lengths.pkl files don't exist"
+            )
+
+        return ss
+
+    @classmethod
+    def load_or_create_embeddings(cls, docs: list):
+        try:
+            ss = SemanticSearch.from_cache(docs)
+        except FileNotFoundError:
+            ss = SemanticSearch()
+            ss.build_embeddings(docs)
+
+        return ss
+
 
 def verify_model():
     ss = SemanticSearch()
     print(f"Model loaded: {ss.model}")
     print(f"Max sequence length: {ss.model.max_seq_length}")
+
+def verify_embeddings():
+    ss = SemanticSearch.load_or_create_embeddings(load_data())
+    print(f"Number of docs: {len(ss.documents)}")
+    print(
+        f"Embeddings shape: {ss.embeddings.shape[0]} vectors in {ss.embeddings.shape[1]} dimensions"
+    )
+
+def embed_text(text: str):
+    embedding = SemanticSearch().generate_embedding(text)
+
+    print(f"Text: {text}")
+    print(f"First 4 dimensions: {embedding[:4]}")
+    print(f"Dimensions: {embedding.shape[0]}")
